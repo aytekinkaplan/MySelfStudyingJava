85d5d0a0386818247b2bad2f280cf081d7f44bcd
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-30 22:03:30 +0300

    Commit

--- src/TDarrays.java
+++ src/TDarrays.java
@@ -5,17 +5,17 @@ public class TDarrays {
 int[][] grades ={{78,67,90,68},{90,69,50,89},{67,78,89,100}};
         System.out.println("1. DİZİN: ");
         for (int i = 0; i < grades[0].length; i++) {
-            System.out.println(grades[0][i]+" ");
+            System.out.print(grades[0][i]+" ");
         }
         System.out.println("\n");
         System.out.println("2. DİZİN: ");
         for (int i = 0; i < grades[1].length; i++) {
-            System.out.println(grades[1][i]+" ");
+            System.out.print(grades[1][i]+" ");
         }
         System.out.println("\n");
         System.out.println("3. DİZİN: ");
         for (int i = 0; i < grades[2].length; i++) {
-            System.out.println(grades[2][i]+" ");
+            System.out.print(grades[2][i]+" ");
         }
     }
 }
