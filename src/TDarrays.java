5167d11ed1302bbc1d48ca5623e3707032afeb1c
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-30 21:34:43 +0300

    Commit

--- src/TDarrays.java
+++ src/TDarrays.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+
 public class TDarrays {
     public static void main(String[] args) {
-
+int[][] grades ={{78,67,90},{90,69,50},{67,78,89}};
+        for (int i = 0; i < grades.length; i++) {
+            System.out.println(Arrays.toString(grades));
+        }
     }
 }
