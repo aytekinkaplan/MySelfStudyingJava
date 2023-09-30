05a11b492d877508fe5754793d995589048f82ea
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-30 21:54:53 +0300

    Commit

--- src/TDarrays.java
+++ src/TDarrays.java
@@ -2,9 +2,18 @@ import java.util.Arrays;
 
 public class TDarrays {
     public static void main(String[] args) {
-int[][] grades ={{78,67,90},{90,69,50},{67,78,89}};
-        for (int i = 0; i < grades.length; i++) {
-            System.out.println(Arrays.toString(grades));
+int[][] grades ={{78,67,90,68},{90,69,50,89},{67,78,89,100}};
+        System.out.println("1. DİZİN: ");
+        for (int i = 0; i < grades[0].length; i++) {
+            System.out.println(grades[0][i]);
+        }
+        System.out.println("2. DİZİN: ");
+        for (int i = 0; i < grades[1].length; i++) {
+            System.out.println(grades[1][i]);
+        }
+        System.out.println("3. DİZİN: ");
+        for (int i = 0; i < grades[2].length; i++) {
+            System.out.println(grades[2][i]);
         }
     }
 }
