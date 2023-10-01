21a8dfcfae622acb4beae0ffe81aceae12d7c223
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-10-01 16:25:22 +0300

    Commit

--- src/OOP/Main3.java
+++ src/OOP/Main3.java
@@ -15,9 +15,11 @@ public class Main3 {
                 ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
 
 
-        /*Car2 toyota = new Car2("Toyota",10000, 2020,"Green");
+        /*
+        Car2 toyota = new Car2("Toyota",10000, 2020,"Green");
         Car2 dodge = new Car2("Dodge", 11000, 2019, "Blue");
         Car2 honda = new Car2("Honda",9000, 2017, "Red");
-        Car2 ford = new Car2("Ford",8000, 2016,"Blue");*/
+        Car2 ford = new Car2("Ford",8000, 2016,"Blue");
+        */
     }
 }
