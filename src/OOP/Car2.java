254c553077126cb0075aad46858e3fed76399c4c
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-10-01 15:09:00 +0300

    Commit

--- src/OOP/Car2.java
+++ src/OOP/Car2.java
@@ -1,12 +1,12 @@
 package OOP;
 
 public class Car2 {
-   private String make;
+    private String make;
     private double price;
     private int year;
     private String color;
 
-    public Car2(String make, double price, int year, String color){
+    public Car2(String make, double price, int year, String color) {
         this.make = make;
         this.price = price;
         this.year = year;
@@ -17,8 +17,16 @@ public class Car2 {
         return make;
     }
 
-    public double getPrice(){
+    public double getPrice() {
         return price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
     }
 
 
